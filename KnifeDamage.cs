a8cd3f70fa66dc4089e56f318ec93e6bdb61c7c5
aquevadis <109932077+aquevadis@users.noreply.github.com>
2024-06-11 19:30:45 +0300

    get attacking player's controller and weapon services

--- KnifeDamage.cs
+++ KnifeDamage.cs
@@ -37,15 +37,25 @@ public class KnifeDamage : BasePlugin
 
         VirtualFunctions.CBaseEntity_TakeDamageOldFunc.Hook(hook =>
         {
+            var entity = hook.GetParam<CEntityInstance>(0);
             var info = hook.GetParam<CTakeDamageInfo>(1);
-            
-            SendMessageToSpecificChat(msg: $"======> BitsDamageType: {(int)info.BitsDamageType}", print: PrintTo.ChatAll);
+
+            var player = Utilities.GetPlayerFromIndex((int)entity.Index + 1);
+            if (player == null || !player.IsValid) 
+                return HookResult.Continue;
+
+            CPlayer_WeaponServices? weaponServices = player.PlayerPawn?.Value?.WeaponServices;
+            if (weaponServices == null) 
+                return HookResult.Continue;
+
+            SendMessageToSpecificChat(msg: $"Player {player.SteamID} fired with weapon ======> BitsDamageType: {(int)info.BitsDamageType}", print: PrintTo.ChatAll);
 
             if (info.BitsDamageType is not 4) return HookResult.Continue;
             info.Damage = 0f;
             
             return HookResult.Changed;
-
+            
+            
         }, HookMode.Pre);
         
         Console.WriteLine("Hooked Take Damage Func! \n!");
@@ -59,8 +69,67 @@ public class KnifeDamage : BasePlugin
         
     }
 
-   
     //helpers
+
+    internal static Dictionary<int, string> WeaponDefIndex { get; } = new Dictionary<int, string>
+	{
+		{ 1, "weapon_deagle" },
+		{ 2, "weapon_elite" },
+		{ 3, "weapon_fiveseven" },
+		{ 4, "weapon_glock" },
+		{ 7, "weapon_ak47" },
+		{ 8, "weapon_aug" },
+		{ 9, "weapon_awp" },
+		{ 10, "weapon_famas" },
+		{ 11, "weapon_g3sg1" },
+		{ 13, "weapon_galilar" },
+		{ 14, "weapon_m249" },
+		{ 16, "weapon_m4a1" },
+		{ 17, "weapon_mac10" },
+		{ 19, "weapon_p90" },
+		{ 23, "weapon_mp5sd" },
+		{ 24, "weapon_ump45" },
+		{ 25, "weapon_xm1014" },
+		{ 26, "weapon_bizon" },
+		{ 27, "weapon_mag7" },
+		{ 28, "weapon_negev" },
+		{ 29, "weapon_sawedoff" },
+		{ 30, "weapon_tec9" },
+		{ 31, "weapon_taser" },
+		{ 32, "weapon_hkp2000" },
+		{ 33, "weapon_mp7" },
+		{ 34, "weapon_mp9" },
+		{ 35, "weapon_nova" },
+		{ 36, "weapon_p250" },
+		{ 38, "weapon_scar20" },
+		{ 39, "weapon_sg556" },
+		{ 40, "weapon_ssg08" },
+		{ 60, "weapon_m4a1_silencer" },
+		{ 61, "weapon_usp_silencer" },
+		{ 63, "weapon_cz75a" },
+		{ 64, "weapon_revolver" },
+		{ 500, "weapon_bayonet" },
+		{ 503, "weapon_knife_css" },
+		{ 505, "weapon_knife_flip" },
+		{ 506, "weapon_knife_gut" },
+		{ 507, "weapon_knife_karambit" },
+		{ 508, "weapon_knife_m9_bayonet" },
+		{ 509, "weapon_knife_tactical" },
+		{ 512, "weapon_knife_falchion" },
+		{ 514, "weapon_knife_survival_bowie" },
+		{ 515, "weapon_knife_butterfly" },
+		{ 516, "weapon_knife_push" },
+		{ 517, "weapon_knife_cord" },
+		{ 518, "weapon_knife_canis" },
+		{ 519, "weapon_knife_ursus" },
+		{ 520, "weapon_knife_gypsy_jackknife" },
+		{ 521, "weapon_knife_outdoor" },
+		{ 522, "weapon_knife_stiletto" },
+		{ 523, "weapon_knife_widowmaker" },
+		{ 525, "weapon_knife_skeleton" },
+		{ 526, "weapon_knife_kukri" }
+	};
+
     private void SendMessageToSpecificChat(CCSPlayerController handle = null!, string msg = "",
         PrintTo print = PrintTo.Chat)
     {
